956fd0ec0503cf8244826f064dcd23637bd750fe
bashiho <bashiho@proton.me>
2026-04-15 11:58:32 -0700

    Added extra value (discipline), attempting adding miagine swapping based on happiness values, also attempted to implement feature where buttons are disabled when the changes would be problematic

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // TBD
 // Add feedback from pet
-// Replace pet image
+// Replace pet image 
+// (maybe done, requires tesintg) at certain happiness values, image changes
 // Add JQuery methods
-// Make it so that at certain happiness values, image changes
-// ADD ANOTHER PROPERTY WITH HAPPINESS AND WEIGHT
+// Improve checks structure
 // https://www.geeksforgeeks.org/javascript/javascript-set-an-image-source-dynamically-using-js/
   
-// Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
-var pet_info = {name:"Scrimblo", weight:"50", happiness:"50"};
+// Add a variable "pet_info" equal to a object with the name (string), 
+// weight (number), happiness (number), and discipline (number) of your pet
+var pet_info = {name:"Scrimblo", weight:"50", happiness:"50", discipline:"50"};
 
 $(function() { // Makes sure that your function is called once all the DOM elements of the page are ready to be used.
     
@@ -20,44 +21,100 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
   $('.play-button').click(clickedPlayButton);
   $('.exercise-button').click(clickedExerciseButton);
   $('.train-button').click(clickedTrainButton);
+
+  // Change image based on happiness
+  let img = document.getElementsByClassName("pet-image");
+  
+  if(pet_info['happiness'] <= 20)
+    img.src = "./images/rem_sad";
+
+  else if(pet_info['happiness'] >= 80)
+    img.src = "./images/rem_happy";
+
+  else
+    img.src = "./images/rem"
+
+  // Gray out buttons if they are unsafe
+  if((pet_info['weight'] - 2 <= 0) || (pet_info['discipline'] -4 < 0)){
+    $('.play-button').attr("disabled", "disabled");
+    $('.play-button').css("background-color", "light-gray");
+  }
+  else{
+    $('.play-button').attr("disabled", "enabled");
+    $('.play-button').css("background-color", "#1e2835");
+  }
+  if((pet_info['happiness'] - 3 < 0) || (pet_info['weight'] - 5 <= 0)){
+    $('.exercise-button').attr("disabled", "disabled");
+    $('.exercise-button').css("background-color", "light-gray");
+  }
+  else{
+    $('.exercise-button').attr("disabled", "enabled");
+    $('.exercise-button').css("background-color", "#1e2835");
+  }
+  if(pet_info['happiness'] - 5 < 0){
+    $('.train-button').attr("disabled", "disabled");
+    $('.train-button').css("background-color", "light-gray");
+  }
+  else{
+    $('.train-button').attr("disabled", "enabled");
+    $('.train-button').css("background-color", "#1e2835");
+  }
 })
 
 // Change values passed through to checkAndUpdatePetInfoInHtml based on button pressed
 function clickedTreatButton() {
-  checkAndUpdatePetInfoInHtml("treat", 5, 5);
+  checkAndUpdatePetInfoInHtml("treat", 5, 5, 0);
 }
 
 function clickedPlayButton() {
-  checkAndUpdatePetInfoInHtml("play", 3, -2);
+  checkAndUpdatePetInfoInHtml("play", 3, -2, -4);
 }
 
 
 function clickedExerciseButton() {
-  checkAndUpdatePetInfoInHtml("exercise", -3, -5);
+  checkAndUpdatePetInfoInHtml("exercise", -3, -5, 2);
 } 
   
 function clickedTrainButton() {
-  checkAndUpdatePetInfoInHtml("train", -5, 2);
+  checkAndUpdatePetInfoInHtml("train", -5, 2, 5);
 }
 
 // uses checkWeightAndHappinessBeforeUpdating to check if values are safe, continues to update info if so
-function checkAndUpdatePetInfoInHtml(action, happinessChange, weightChange) {
-  var change = checkWeightAndHappinessBeforeUpdating(action, happinessChange, weightChange);  
+function checkAndUpdatePetInfoInHtml(action, happinessChange, weightChange, discChange) {
+  var change = checkWeightAndHappinessBeforeUpdating(action, happinessChange, weightChange, discChange);  
   if(change){
+    $('.warning').text("Cannot " + action + ", happiness and weight are too low!");
     pet_info['happiness'] += happinessChange;
     pet_info['weight'] += weightChange;
+    pet_info['discipline'] += discChange;
     updatePetInfoInHtml(happinessChange, weightChange);
   }
 }
 
-// Checks if values are safe to use before acting, returns true if safe false otherwise
-function checkWeightAndHappinessBeforeUpdating(action, happinessChange, weightChange) {
-  if((pet_info['happiness'] += happinessChange < 0) && (pet_info['weight'] += weightChange <= 0)){
+// Checks if values are safe to use before acting, returns true if safe , returns false and prints warning otherwise
+function checkWeightAndHappinessBeforeUpdating(action, happinessChange, weightChange, discChange) {
+  if((pet_info['happiness'] += happinessChange < 0) && (pet_info['weight'] += weightChange <= 0) && (pet_info['discipline'] += discChange < 0)){
+    $('.warning').text("Cannot " + action + ", discipline, happiness and weight are too low!");
+    return false;
+  }
+  else if(pet_info['happiness'] += happinessChange < 0 && (pet_info['weight'] += weightChange <= 0)){
     $('.warning').text("Cannot " + action + ", happiness and weight are too low!");
     return false;
   }
+  else if(pet_info['weight'] += weightChange <= 0 && (pet_info['discipline'] += discChange < 0)) {
+    $('.warning').text("Cannot " + action + ", discipline and weight are too low!");
+    return false;
+  }
+  else if((pet_info['happiness'] += happinessChange < 0) && (pet_info['discipline'] += discChange < 0)){
+    $('.warning').text("Cannot " + action + ", discipline and happiness are too low!");
+    return false;
+  }
   else if(pet_info['happiness'] += happinessChange < 0){
-    $('.warning').text("Cannot " + action + ", happiness is too low!");
+    $('.warning').text("Cannot " + action + ", happiness too low!");
+    return false;
+  }
+  else if(pet_info['discipline'] += discChange < 0){
+    $('.warning').text("Cannot " + action + ", discipline is too low!");
     return false;
   }
   else if(pet_info['weight'] += weightChange <= 0) {
@@ -72,4 +129,5 @@ function updatePetInfoInHtml() {
   $('.name').text(pet_info['name']);
   $('.weight').text(pet_info['weight']);
   $('.happiness').text(pet_info['happiness']);
+  $('.discipline').text(pet_info['discipline']);
 }
